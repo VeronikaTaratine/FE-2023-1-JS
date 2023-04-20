ae8896f6cd4ad370f506fbac8b546d2fa3d5951b
Veronika Taratine <veronika.taratine@gmail.com>
2023-04-19 22:54:30 -0400

    array

---
+++ assets/js/index.js
@@ -0,0 +1,18 @@
+const numbers=[1,2,3,
+];
+numbers.push(6,5,4);
+numbers.unshift(-3,-2,-1);
+
+
+const numbers2=[100,-273.15,0,1,2,3,4,5,6,9,10];
+const deletedNumbers3=numbers2.pop();
+const deletedNumbers4=numbers2.shift();
+
+
+const numbers5=[1,2,3,4,5];
+const resalt= numbers5.splice(-4,2);
+
+const number6=[1,2,3,4,5];
+const resalt1=number6.splice(-2,1,'a','b','c');
+
+const number7=[1,2,3,4,5];
